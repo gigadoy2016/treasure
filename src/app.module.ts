fb2491a0fe94ab9b9df22920e8545a9102f09794
gigadoy2016 <rangsan_m@hotmail.com>
2021-03-21 17:11:54 +0700

    add mongodb

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,9 +1,10 @@
 import { Module } from '@nestjs/common';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
+import { StoragesModule } from './storages/storages.module';
 
 @Module({
-  imports: [],
+  imports: [StoragesModule],
   controllers: [AppController],
   providers: [AppService],
 })
